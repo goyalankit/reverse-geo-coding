853ab2928fbc4facadf4d4b7a288cd728fde11b8
ankitunique <ankitunique@yahoo.com>
2012-12-01 23:22:31 +0530

    adding a little bit of css

--- application.rb
+++ application.rb
@@ -13,16 +13,19 @@ class ReverseGeocode < Sinatra::Base
   end
 
   post "/upload" do
-    File.open('uploads/' + params['myfile'][:filename], "w") do |f|
+    File.open('public/uploads/' + params['myfile'][:filename], "w") do |f|
       f.write(params['myfile'][:tempfile].read)
     end
     @@filename = params['myfile'][:filename]
+    @filename = @@filename
     haml :success
   end
 
   get "/reverse_geo_code_it" do
-    reverse_geo_code("uploads/#{@@filename}", "uploads/#{params["output_filename"]}")
-    return "done successfully"
+    reverse_geo_code("public/uploads/#{@@filename}", "public/uploads/#{params['output_filename']}.csv")
+    @count = %x{wc -l "public/uploads/#{@@filename}"}.split.first.to_i
+    @url = "uploads/#{params['output_filename']}.csv"
+    haml :output
   end
 end
 
@@ -30,8 +33,13 @@ def reverse_geo_code input_file_name, output_file_name
   output_file_name = output_file_name || "reverse_geo_coded_#{input_file_name}"
   header = "place_id|line4| line3| line2| line1| offsetlat| county| house| offsetlon| countrycode| postal| longitude| state| street| country| latitude| cross| radius| quality| city| neighborhood"
   File.open(output_file_name, 'w') { |f| f.write("#{header}\n") }
-
+  count = 0
   CSV.foreach(input_file_name) do |row|
+    count += 1
+    if count == 300
+      sleep(10)
+      count = 0
+    end
     latitude  = row[0]
     longitude = row[1]
     place_id  = row[2]
