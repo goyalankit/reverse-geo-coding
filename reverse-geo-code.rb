2e160d90685f1f71ec2ebbc74a47ca203500b55c
ankitunique <ankitunique@yahoo.com>
2012-12-09 18:46:04 +0530

    adding sleep time

--- reverse-geo-code.rb
+++ reverse-geo-code.rb
@@ -20,13 +20,18 @@ CSV.foreach(input_file_name) do |row|
   longitude = row[1]
   place_id  = row[2]
   response  = Faraday.get "http://where.yahooapis.com/geocode?q=#{latitude},#{longitude}&gflags=AR&flags=J&appid=#{ENV['app_id']}"
-  json_response    =  JSON.parse(response.body)["ResultSet"]
+  begin
+    json_response    =  JSON.parse(response.body)["ResultSet"]
+  rescue
+    p "error"
+  end
   pbar.increment
-  if json_response["Found"] > 0
+  if json_response["Found"].to_i > 0
     result = json_response["Results"].first
     csv ="#{place_id}|#{result['line4']}| #{result['line3']}| #{result['line2']}| #{result['line1']}| #{result['offsetlat']}| #{result['county']}| #{result['house']}| #{result['offsetlon']}| #{result['countrycode']}| #{result['postal']}| #{result['longitude']}| #{result['state']}| #{result['street']}| #{result['country']}| #{result['latitude']}| #{result['cross']}| #{result['radius']}| #{result['quality']}| #{result['city']}| #{result['neighborhood']}" 
     File.open(output_file_name, 'a') { |f| f.write("#{csv}\n") }
   else
     puts "#{json_response.merge( {:latitude => latitude, :longitude => longitude})}\n"
   end
+  sleep(7)
 end
